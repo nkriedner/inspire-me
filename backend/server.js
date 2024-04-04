e43947c04c3231663dab4194817ef04379c9020a
Nils Kriedner <nkriedner@googlemail.com>
2024-04-04 23:56:51 +0200

    Install mongoose & connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 // IMPORT NPM PACKAGES
 require("dotenv").config(); // enables the use of environment variables
 const express = require("express");
+const mongoose = require("mongoose"); // ODM library for MongoDB
 
 // IMPORT ROUTES
 const inspirationRoutes = require("./routes/inspirationRoutes");
@@ -18,7 +19,17 @@ app.use((req, res, next) => {
 // USE IMPORTED EXPRESS ROUTES
 app.use("/api/inspirations", inspirationRoutes);
 
+// CONNECT TO DATABASE
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT || 4000, () => {
+            console.log(`Connected to database & listening on port ${process.env.PORT}.`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
 // LISTEN TO REQUESTS
-app.listen(process.env.PORT || 4000, () => {
-    console.log(`Server listening on port ${process.env.PORT}.`);
-});
