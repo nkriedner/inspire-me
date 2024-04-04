a02857e4a1316497615633160373cff46a11771c
Nils Kriedner <nkriedner@googlemail.com>
2024-04-04 23:07:02 +0200

    Enable the use of environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 // IMPORT NPM PACKAGES
+require("dotenv").config(); // enables the use of environment variables
 const express = require("express");
 
 // CREATE EXPRESS APP
 const app = express();
 
+// EXPRESS ROUTES
+app.get("/", (req, res) => {
+    res.json({ mssg: "Welcome to Inspire-Me!" });
+});
+
 // LISTEN TO REQUESTS
-app.listen(4000, () => {
-    console.log("Server listening on port 4000.");
+app.listen(process.env.PORT || 4000, () => {
+    console.log(`Server listening on port ${process.env.PORT}.`);
 });
