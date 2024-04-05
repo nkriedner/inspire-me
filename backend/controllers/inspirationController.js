f846e6848c50e5eb875a672dd30eb8d385b904a2
Nils Kriedner <nkriedner@googlemail.com>
2024-04-05 10:51:29 +0200

    Create inspiration controller functions

---
+++ backend/controllers/inspirationController.js
@@ -0,0 +1,107 @@
+// IMPORT NPM PACKAGES
+const mongoose = require("mongoose"); // ODM library for MongoDB
+
+// IMPORT INSPIRATION MODEL
+const Inspiration = require("../models/InspirationModel");
+
+// INSPIRATION CONTROLLER FUNCTIONS
+// GET all inspirations
+const getInspirations = async (req, res) => {
+    // find all inspiration sorted by creation date
+    const inspirations = await Inspiration.find({}).sort({ createdAt: -1 });
+
+    // send 'success' response status (200) with data in json
+    res.status(200).json(inspirations);
+};
+
+// GET a single inspiration
+const getInspiration = async (req, res) => {
+    const { id } = req.params; // destructures the id from the request params
+
+    // check if id is (not) a valid mongoose id
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        // if it is not valid -> send 'bad request' response status (400) with error message
+        return res.status(404).json({ error: "The id is not a valid mongoose id." });
+    }
+
+    // find the inspiration via its id
+    const inspiration = await Inspiration.findById(id);
+
+    // check if inspiration exists
+    if (!inspiration) {
+        // if it does not exist -> send 'bad request' response status (400) with error message
+        return res.status(404).json({ error: "No inspiration with the corresponding id in database." });
+    }
+
+    // send 'success' response status (200) with data in json
+    res.status(200).json(inspiration);
+};
+
+// CREATE new inspiration
+const createInspiration = async (req, res) => {
+    const { content, source, category } = req.body; // destructures the data from the request body
+
+    try {
+        // create new inspiration (= add a document to database)
+        const inspiration = await Inspiration.create({ content, source, category });
+        // send 'success' response status (200) with data in json
+        res.status(200).json(inspiration);
+    } catch (error) {
+        // send 'bad request' response status (400) with message text of the error object
+        res.status(400).json({ error: error.message });
+    }
+};
+
+// DELETE an inspiration
+const deleteInspiration = async (req, res) => {
+    const { id } = req.params; // destructures the id from the request params
+
+    // check if id is (not) a valid mongoose id
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        // if it is not valid -> send 'bad request' response status (400) with error message
+        return res.status(404).json({ error: "The id is not a valid mongoose id." });
+    }
+
+    // find the inspiration via its id and delete it
+    const inspiration = await Inspiration.findOneAndDelete({ _id: id });
+
+    // check if inspiration exists
+    if (!inspiration) {
+        // if it does not exist -> send 'bad request' response status (400) with error message
+        return res.status(404).json({ error: "No inspiration with the corresponding id in database." });
+    }
+
+    // send 'success' response status (200) with data in json
+    res.status(200).json(inspiration);
+};
+
+// UPDATE an inspiration
+const updateInspiration = async (req, res) => {
+    const { id } = req.params; // destructures the id from the request params
+
+    // check if id is (not) a valid mongoose id
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        // if it is not valid -> send 'bad request' response status (400) with error message
+        return res.status(404).json({ error: "The id is not a valid mongoose id." });
+    }
+
+    // find the inspiration via its id and update it
+    const inspiration = await Inspiration.findOneAndUpdate(
+        { _id: id },
+        {
+            ...req.body,
+        }
+    );
+
+    // check if inspiration exists
+    if (!inspiration) {
+        // if it does not exist -> send 'bad request' response status (400) with error message
+        return res.status(404).json({ error: "No inspiration with the corresponding id in database." });
+    }
+
+    // send 'success' response status (200) with data in json
+    res.status(200).json(inspiration);
+};
+
+// EXPORT CONTROLLER FUNCTIONS
+module.exports = { getInspirations, getInspiration, createInspiration, deleteInspiration, updateInspiration };
--- backend/routes/inspirationRoutes.js
+++ backend/routes/inspirationRoutes.js
@@ -1,43 +1,29 @@
 // IMPORT NPM PACKAGES
 const express = require("express");
 
-// IMPORT INSPIRATION MODEL
-const Inspiration = require("../models/InspirationModel");
+// IMPORT CONTROLLER FUNCTIONS
+const {
+    getInspirations,
+    getInspiration,
+    createInspiration,
+    deleteInspiration,
+    updateInspiration,
+} = require("../controllers/inspirationController");
 
 // CREATE EXPRESS ROUTER
 const router = express.Router();
 
 // ROUTES
 // GET all inspirations
-router.get("/", (req, res) => {
-    res.json({ mssg: "GET all inspirations" });
-});
+router.get("/", getInspirations);
 // GET a single inspiration
-router.get("/:id", (req, res) => {
-    res.json({ mssg: "GET a single inspiration" });
-});
+router.get("/:id", getInspiration);
 // POST a new inspiration
-router.post("/", async (req, res) => {
-    const { content, source, category } = req.body; // destructures the data from the request body
-
-    try {
-        // create new inspiration
-        const inspiration = await Inspiration.create({ content, source, category });
-        // send 'success' response status (200) with data in json
-        res.status(200).json(inspiration);
-    } catch (error) {
-        // send 'bad request' response status (400) with message text of the error object
-        res.status(400).json({ error: error.message });
-    }
-});
+router.post("/", createInspiration);
 // DELETE an inspiration
-router.delete("/:id", (req, res) => {
-    res.json({ mssg: "DELETE an inspiration" });
-});
+router.delete("/:id", deleteInspiration);
 // UPDATE an inspriation
-router.patch("/:id", (req, res) => {
-    res.json({ mssg: "UPDATE an inspiration" });
-});
+router.patch("/:id", updateInspiration);
 
 // EXPORT THE ROUTER
 module.exports = router;
